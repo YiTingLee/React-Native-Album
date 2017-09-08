6ae2b3eef37bf94b319fca92cc573085ac347fc4
yitinglee <ranye0719@gmail.com>
2017-09-08 21:00:52 +0800

    Displaying individual albums

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -1,19 +1,29 @@
 //Import libraries for making a component
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './albumDetail';
 
 //Make a component
 class AlbumList extends Component {
+    state = { albums: [] };
+
     componentWillMount() {
         axios.get('http://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
+        );
     }
 
     render() {
+        console.log(this.state);
         return (
             <View>
-                <Text>AlbumList</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
